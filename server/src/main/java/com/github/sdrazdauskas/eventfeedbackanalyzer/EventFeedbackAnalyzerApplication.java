a774e86710ba0e195731998da25b917788738bf2
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2025-06-11 19:27:17 +0300

    Initial commit

---
+++ server/src/main/java/com/github/sdrazdauskas/eventfeedbackanalyzer/EventFeedbackAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.github.sdrazdauskas.eventfeedbackanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFeedbackAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventFeedbackAnalyzerApplication.class, args);
+    }
+
+}
