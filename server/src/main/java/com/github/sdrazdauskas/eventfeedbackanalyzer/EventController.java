fa3325002a1bcac5d31ffcf00af87c1d23b8e448
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2025-06-12 19:42:53 +0300

    fix cors error from backend

--- server/src/main/java/com/github/sdrazdauskas/eventfeedbackanalyzer/EventController.java
+++ server/src/main/java/com/github/sdrazdauskas/eventfeedbackanalyzer/EventController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-
+@CrossOrigin
 @OpenAPIDefinition(
     info = @Info(title = "Event Feedback Analyzer API", version = "1.0", description = "API for event feedback and sentiment analysis.")
 )
