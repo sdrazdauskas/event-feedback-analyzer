6f8d39ec7caec6690796ff8eff6a44a51c983702
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2025-06-13 18:46:11 +0300

    add environment files for API url(dev and production)

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Event, Feedback, SentimentSummary } from './models';
+import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-  private apiUrl = 'http://localhost:8080/events';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://event-feedback-analyzer-387154959779.europe-west1.run.app/events'
+};
