ce4d8ddc83444599775afb2494a658d13694acf2
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2025-06-14 11:03:21 +0300

    fix failing testing due title name

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -21,6 +21,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Event Feedback Analyzer');
+    expect(compiled.querySelector('h1')?.textContent).toContain('EventSync');
   });
 });
