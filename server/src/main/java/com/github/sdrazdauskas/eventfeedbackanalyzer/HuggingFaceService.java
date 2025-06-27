edb634bff3ff3b6f6ebd1db0b29504c37ea96033
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2025-06-27 19:41:07 +0300

    change make FeedbackService into interface and implement in HuggingFaceService

--- server/src/main/java/com/github/sdrazdauskas/eventfeedbackanalyzer/HuggingFaceService.java
+++ server/src/main/java/com/github/sdrazdauskas/eventfeedbackanalyzer/HuggingFaceService.java
@@ -1,6 +1,8 @@
 package com.github.sdrazdauskas.eventfeedbackanalyzer;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -12,7 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class HuggingFaceService {
+public class HuggingFaceService implements FeedbackService {
     @Value("${huggingface.api.key}")
     private String key;
     @Value("${huggingface.api.sentiment.url}")
@@ -34,7 +36,48 @@ public class HuggingFaceService {
         this.url = url;
     }
 
-    
+    @Override
+    public Feedback createFeedback(String text) {
+        Sentiment sentiment = analyzeSentiment(text);
+        Feedback feedback = new Feedback(text, sentiment);
+        return feedback;
+    }
+
+    @Override
+    public Sentiment analyzeSentiment(String text) {
+        ResponseEntity<Object> response = postToHuggingFace(text);
+        Object result = response != null ? response.getBody() : null;  
+        if (result instanceof List<?> list && !list.isEmpty()) {
+            Object first = list.get(0);
+            if (first instanceof List<?> resultList && !resultList.isEmpty()) {
+                // Fix max scored label
+                int maxStars = 0;
+                double maxScore = -1;
+                for (Object item : resultList) {
+                    if (item instanceof Map<?, ?> itemMap) {
+                        int currentStars = Integer.parseInt(((String) itemMap.get("label")).split(" ")[0]);
+                        double score = ((Number) itemMap.get("score")).doubleValue();
+                        if (score > maxScore) {
+                            maxScore = score;
+                            maxStars = currentStars;
+                        }
+                    }
+                }
+                if (maxStars != 0) {
+                    if (maxStars < 3) {
+                        return Sentiment.Negative;
+                    } else if (maxStars == 3) {
+                        return Sentiment.Neutral;
+                    } else {
+                        return Sentiment.Positive;
+                    }
+                }
+            }
+        }
+        System.out.println("[DEBUG] Received non valid response from SentimentModelClient: " + result);
+        return Sentiment.Neutral; // Default to Neutral if there was no valid response
+    }
+
     public ResponseEntity<Object> postToHuggingFace(String text) {
         return postToHuggingFace(text, getUrl(), getKey());
     }
